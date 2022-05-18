d3b515b009615a7f321cae80fe381dbafab59c8e
ho-okim <khk821910@naver.com>
2022-05-18 19:11:06 +0000

    Update dir, main.js, navigation.ejs

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const port = 80,
-	http = require("http"),
-	httpStatus = require("http-status-codes"),
-	app = http.createServer((request, response) => {
-		console.log("Received an incoming request!");
-		response.writeHead(httpStatus.OK, {
-			"Content-Type": "text/html"
-		});
+	express = require("express"),
+	app = express();
 
-		let responseMessage = "<h1>Hello, Universe!</h1>";
-		response.write(responseMessage);
-		response.end();
-		console.log(`Sent a response : ${responseMessage}`);
-	});
+app.get("/", (req, res) => {
+	res.send("Hello, Universe!");
+})
 
-app.listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+app.use(
+	express.urlencoded({
+		extended: false
+	})
+);
+app.use(express.json());
+
+.listen(port, () => {
+	console.log(`The Express.js server: ${port}`);
+});
