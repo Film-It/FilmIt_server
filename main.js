e900a99ad3223a84c46aadea262d2ce584b699d0
Gahyeon <ghkim7319@gmail.com>
2022-06-01 07:12:43 +0000

    Revise

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.use(
 );
 app.use(express.json());
 app.use(passport.initialize());
-app.use(passport.session);
+app.use(passport.session());
 
 db.sequelize.sync({ force: true })
   .then(() => {
