6634c11ca25340b39069e46e1e00be3ef461c70b
Gahyeon <ghkim7319@gmail.com>
2022-06-01 07:31:06 +0000

    Reset

--- main.js
+++ main.js
@@ -33,61 +33,9 @@ db.sequelize.sync({ force: true })
     console.error(err);
   });
 
-  //로그인 인증 (Passport)
-passport.use(new LocalStrategy({
-	//로그인 페이지 input 태그 내 name
-	usernameField: 'email',
-	passwordField: 'passwd'
-},
-(id, password, done)=>{
-  console.log(id, password);
-//회원 정보가 한개이상 있을때
-if(user){
-  console.log(user);
-
-	//아이디가 다를때
-	if (id !== user.email)  
-		return done(null, false, { message: '아이디가 다르다' });
-	//비밀번호가 다를때
-	else if (password !== user.password) 
-		return done(null, false, { message: '비번이 다르다' });
-	//아이디, 비밀번호 모두 맞을 경우
-	return done(null, user);
-}
-}));
-
-passport.serializeUser(function(user, done) {   //쓰기
-    done(null, user.email);
-});
-
-passport.deserializeUser(function(id, done) {   //읽기
-    done(null, id);
-});
-
 app.get("/name", homeController.respondWithName);
 app.get("/login", loginController.showLogin);
-// app.post("/login", loginController.postedLogin);
-app.post("/login", (req,res, next)=> {
-    passport.authenticate('local', (err, user, info) => {
-        if(err){
-            console.error(err);
-            return next(err);
-        }
-        if(info){
-            return res.status(401).send(info.reason);
-        }
-        
-        // passport.login
-        return req.login(user, async(loginErr) => {
-            if(loginErr){
-                console.error(loginErr);
-                return next(loginErr);
-            }
-
-            return res.json(user);
-        })
-    })(req, res, next);
-});
+app.post("/login", loginController.postedLogin);
 app.get("/users", userController.getAllUsers);
 app.get("/signup", signupController.showSignup);
 app.post("/signup", signupController.postedSignup);
