ffac7362f711f0cd656ef67a1e55b9c1238c5b20
Gahyeon <ghkim7319@gmail.com>
2022-06-01 06:41:59 +0000

    Add passport

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,6 @@
+
+const passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+
 exports.showLogin = (req, res) => {
         res.render("login");
 };
@@ -7,3 +10,10 @@ exports.postedLogin = (req, res) => {
 	let passwd = req.body.passwd;
 	res.send("POST Successful!");
 };
+
+// exports.passport.authenticate('local', {
+//     //성공시, 메인페이지 이동
+//     //실패시 로그인 페이지 이동
+//     successRedirect: '/',
+//     failureRedirect: '/login'
+// })
