a3d9e57c3c69a2dfd38968463a70aca9dbf98167
ho-okim <khk821910@naver.com>
2022-06-05 08:23:02 +0000

    Add passport

---
+++ passport/index.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const local = require('./local');
+const db = require("../models/index"),
+User = db.User;
+
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        done(null, user.id); //세션에 id만 저장
+    });
+
+    // id로 유저 찾기
+    passport.deserializeUser(async (id, done) => {
+        try{
+            const user = await User.findOne({ where : { id }});
+            done(null, user);
+
+        }catch(error){
+            console.error(error);
+            done(error);
+        }
+    });
+
+    local();
+};
